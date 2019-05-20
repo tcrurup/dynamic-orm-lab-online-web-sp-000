0cea8ec0ddc0ac282a72285d23d86e3a0da3e6ef
Anthony Rurup <tcrurup@gmail.com>
2019-05-20 23:26:04 +0000

    Automatically backed up by Learn

--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -1,5 +1,7 @@
-  1) Student has instance methods to insert data into db #save saves the student to the db
-     Failure/Error: expect(DB[:conn].execute("SELECT * FROM students WHERE name= 'Sam'")).to eq([{"id"=>1, "name"=>"Sam", "grade"=>11, 0=>1, 1=>"Sam", 2=>11}])
+require_relative "../config/environment.rb"
+require 'active_support/inflector'
 
-       expected: [{"id"=>1, "name"=>"Sam", "grade"=>11, 0=>1, 1=>"Sam", 2=>11}]
-            got: [{"grade"=>11, "id"=>1, "name"=>"Sam"}]
+class InteractiveRecord
+
+ 
+end
