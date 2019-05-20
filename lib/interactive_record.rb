70bf6bdb552901c498bd8170fd1e9b5826ed3a0c
Anthony Rurup <tcrurup@gmail.com>
2019-05-20 23:56:47 +0000

    Automatically backed up by Learn

--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -43,7 +43,7 @@ class InteractiveRecord
     column_names
   end
   
-  def self.find_by(column_name, value)
+  def self.find_by(column_name)
     binding.pry
     sql = "SELECT * FROM #{self.table_name} WHERE ? = ?"
     DB[:conn].execute(sql, column_name, value)
